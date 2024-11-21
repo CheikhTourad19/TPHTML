8507375bb7dd7e9f410df4d7000bd25f0d81de97
Cheikh TOURAD <elghothvadel@gmail.com>
2024-11-21 16:07:04 +0100

    Atelier 2 php

--- TP2/EX2/ex2.php
+++ TP2/EX2/ex2.php
@@ -1,5 +1,55 @@
 <?php
-$html= <<<HTML
+
+function minimum($tab)
+{
+    $min = $tab[0];
+    foreach ($tab as $key => $value) {
+        if ($value < $min) {
+            $min = $value;
+        }
+    }
+    return $min;
+}
+
+function maximum($tab)
+{
+    $max = $tab[0];
+    foreach ($tab as $key => $value) {
+        if ($value > $max) {
+            $max = $value;
+        }
+    }
+}
+//
+////function trier(&$tab)
+////{
+////    $tab2=$tab;
+////    $i=0;
+////    var_dump($tab);
+////    foreach ($tab2 as $key => $value) {
+////        $key=$i;
+////        $i++;
+////    }
+////    var_dump($tab2);
+////    for ($j = 0; $j < count($tab2)-1; $j++) {
+////        for ($k = $i; $k < count($tab2); $k++) {
+////            if ($tab2[$k]  > $tab[$k+1]) {
+////                $tmp=$tab2[$k];
+////                $tab2[$k]=$tab[$k+1];
+////                $tab[$k+1]=$tmp;
+////            }
+////        }
+////    }
+////    var_dump($tab2);
+////    $i=0;
+////    foreach ($tab as $key => $value) {
+////        $value=$tab2[$i];
+////        $i++;
+////    }
+//
+//}
+
+$html = <<<HTML
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,39 +61,29 @@ $html= <<<HTML
 <div id="content">
 HTML;
 
-if (isset($_GET['traitement'])&&isset($_GET['num1'])&&isset($_GET['num2'])&&isset($_GET['num3'])&&isset($_GET['num4'])&&isset($_GET['num5'])) {
-    if (!empty($_GET['traitement'])&&!empty($_GET['num1'])&&!empty($_GET['num2'])&&!empty($_GET['num3'])&&!empty($_GET['num4'])&&!empty($_GET['num5'])) {
-        $tab=[$_GET['num1'],$_GET['num2'],$_GET['num3'],$_GET['num4'],$_GET['num5']];
-        if ($_GET['traitement']=='tri') {
+if (isset($_GET['traitement']) && isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['num3']) && isset($_GET['num4']) && isset($_GET['num5'])) {
+    if (!empty($_GET['traitement']) && !empty($_GET['num1']) && !empty($_GET['num2']) && !empty($_GET['num3']) && !empty($_GET['num4']) && !empty($_GET['num5'])) {
+        $tab = [$_GET['num1'], $_GET['num2'], $_GET['num3'], $_GET['num4'], $_GET['num5']];
+        if ($_GET['traitement'] == 'tri') {
 
             sort($tab);
-            $html.="<h1>Voici le tableau trie</h1><table>";
-            foreach ($tab as $key=>$value) {
+            $html .= "<h1>Voici le tableau trie</h1><table>";
+            foreach ($tab as $key => $value) {
                 $key++;
-                $html.="<tr><td>$key</td><td>$value</td></tr>";
+                $html .= "<tr><td>$key</td><td>$value</td></tr>";
             }
-            $html.="</table>";
-        }elseif ($_GET['traitement']=='min') {
-            $min=$_GET['num1'];
-            foreach ($tab as $key=>$value) {
-                if($value<$min) {
-                    $min=$value;
-                }
-            }
-            $html.="<h1>le minimum est :  $min</h1>";
-        }else{
-            $max=$_GET['num1'];
-            foreach ($tab as $key=>$value) {
-                if($value>$max) {
-                    $max=$value;
-                }
-            }
-            $html.="<h1>le maximum est :  $max</h1>";
+            $html .= "</table>";
+        } elseif ($_GET['traitement'] == 'min') {
+            $min = minimum($tab);
+            $html .= "<h1>le minimum est :  $min</h1>";
+        } else {
+            $max = maximum($tab);
+            $html .= "<h1>le maximum est :  $max</h1>";
         }
-    }else{
-        $html.="<h1>Nombres Manquants Verifier votre Choix</h1>";
+    } else {
+        $html .= "<h1>Nombres Manquants Verifier votre Choix</h1>";
     }
-}else{
-    $html.="<h1>Erreur de transmission</h1>";
+} else {
+    $html .= "<h1>Erreur de transmission</h1>";
 }
-echo $html."</div></body></html>";
+echo $html . "</div></body></html>";
